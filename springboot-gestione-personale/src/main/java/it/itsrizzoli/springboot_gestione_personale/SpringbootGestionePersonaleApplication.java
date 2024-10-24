e4729925cb18b4bead771d1ec3c45011c94d22cb
LeandroBenaglio <157703205+LeandroBenaglio@users.noreply.github.com>
2024-10-24 11:16:31 +0200

    Added CIAO

--- springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/SpringbootGestionePersonaleApplication.java
+++ springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/SpringbootGestionePersonaleApplication.java
@@ -8,6 +8,7 @@ public class SpringbootGestionePersonaleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootGestionePersonaleApplication.class, args);
+		System.out.println("CIAO");
 	}
 
 }
