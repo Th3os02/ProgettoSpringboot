f8aa7646fa10976dd30e9ec0c658bdbe56b2159d
Th3os02 <157703336+Th3os02@users.noreply.github.com>
2024-10-24 11:52:53 +0200

    aggiunta attributi nella classe Personale

--- springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Modelli/Personale.java
+++ springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Modelli/Personale.java
@@ -1,4 +1,61 @@
 package it.itsrizzoli.springboot_gestione_personale.Modelli;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Personale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    //Ruolo ruolo
+    //Lingua lingua
+    //Contratto contratto
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
