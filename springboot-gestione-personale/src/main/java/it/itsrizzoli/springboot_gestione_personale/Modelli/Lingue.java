85268b19bb2b78a224759bbe04d388dc2374abb1
LeandroBenaglio <157703205+LeandroBenaglio@users.noreply.github.com>
2024-10-24 12:17:59 +0200

    Fix _seq table creation

--- springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Modelli/Lingue.java
+++ springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Modelli/Lingue.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Lingue {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nomeLingua;
 
--- springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Modelli/OrarioLavoro.java
+++ springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Modelli/OrarioLavoro.java
@@ -11,7 +11,7 @@ import java.time.LocalTime;
 @Entity
 public class OrarioLavoro {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private LocalTime oraInizio;
     private LocalTime oraFine;
