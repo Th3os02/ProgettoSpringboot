61976f6279ff2a34aa2483b5ac8a24220e81dc7c
DiegoBendinelliITS <diego.bendinelli@itsrizzoli.it>
2024-10-24 16:06:17 +0200

    controller eventi con più cose
    Per ora è concettuale, ma la direzione c'è

--- springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Controller/ControllerEventi.java
+++ springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Controller/ControllerEventi.java
@@ -1,7 +1,12 @@
 package it.itsrizzoli.springboot_gestione_personale.Controller;
 
+import it.itsrizzoli.springboot_gestione_personale.Modelli.Personale;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ControllerEventi {
+
+    @GetMapping("eventi")
+    public String eventi(Personale utente) {return "eventi-" + utente.getNome() +"-"+utente.getCognome();}
 }
