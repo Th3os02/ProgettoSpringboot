d01156356bc34b4c39e812bfed39dc091e11ef88
GiacomoGrieco <giacomo.grieco@itsrizzoli.it>
2024-10-24 10:55:38 +0200

    Inizio progetto

---
+++ springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/SpringbootGestionePersonaleApplication.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.springboot_gestione_personale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGestionePersonaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGestionePersonaleApplication.class, args);
+	}
+
+}
---
+++ springboot-gestione-personale/src/test/java/it/itsrizzoli/springboot_gestione_personale/SpringbootGestionePersonaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.springboot_gestione_personale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGestionePersonaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
