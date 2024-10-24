33ab3cd9f3523b14bcced57e68bc9e1d10615518
GiacomoGrieco <giacomo.grieco@itsrizzoli.it>
2024-10-24 15:36:20 +0200

    Aggiunta pagina listapersonale

--- springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Controller/ControllerPersonale.java
+++ springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Controller/ControllerPersonale.java
@@ -1,10 +1,17 @@
 package it.itsrizzoli.springboot_gestione_personale.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class ControllerPersonale {
     @GetMapping("/")
     public String login() {
         return "Login";
+    }
+
+    @GetMapping("gestisci")
+    public String ListaPers(Model model) {
+        model.addAttribute("", utentiSalvati);
+        return "ListaPersonale";
     }
 }
