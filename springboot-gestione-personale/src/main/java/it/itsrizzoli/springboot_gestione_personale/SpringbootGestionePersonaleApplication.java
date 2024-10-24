327951ea318332e2534577d77dbd49e5daedb65c
LeandroBenaglio <157703205+LeandroBenaglio@users.noreply.github.com>
2024-10-24 11:25:43 +0200

    Remove CIAO

--- springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/SpringbootGestionePersonaleApplication.java
+++ springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/SpringbootGestionePersonaleApplication.java
@@ -8,7 +8,6 @@ public class SpringbootGestionePersonaleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootGestionePersonaleApplication.class, args);
-		System.out.println("CIAO");
 	}
 
 }
