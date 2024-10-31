754e94b5603fde8d2099e1eacfba7a51726bc615
Th3os02 <157703336+Th3os02@users.noreply.github.com>
2024-10-31 17:38:23 +0100

    modifica in PersonaleReposity

--- springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/DAO/PersonaleRepository.java
+++ springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/DAO/PersonaleRepository.java
@@ -5,7 +5,12 @@ import it.itsrizzoli.springboot_gestione_personale.Modelli.Personale;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PersonaleRepository extends CrudRepository<Personale, Integer> {
    @Query("select p from Personale p where email = :email and password = :password")
     public Personale login(String email, String password);
+
+    List<Personale> findByEmail(String email); //select * from Personale when email = :email
+   Personale findById(long id);
 }
