4288a3986cc7bc1f2d39ced8a8de182dec71b5cc
LeandroBenaglio <157703205+LeandroBenaglio@users.noreply.github.com>
2024-11-02 13:42:29 +0100

    Add functioning session; Login (session) is required to access to HomePage, else /login is shown.

--- springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Controller/ControllerHomePage.java
+++ springboot-gestione-personale/src/main/java/it/itsrizzoli/springboot_gestione_personale/Controller/ControllerHomePage.java
@@ -2,27 +2,18 @@ package it.itsrizzoli.springboot_gestione_personale.Controller;
 
 import it.itsrizzoli.springboot_gestione_personale.DAO.PersonaleRepository;
 import it.itsrizzoli.springboot_gestione_personale.Modelli.Personale;
-import it.itsrizzoli.springboot_gestione_personale.Modelli.Ruolo;
 import it.itsrizzoli.springboot_gestione_personale.DAO.RuoloRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-
-import static it.itsrizzoli.springboot_gestione_personale.Controller.ControllerPersonale.listaPersonale;
 
 
 @Controller
 public class ControllerHomePage {
-
-@Autowired
-RuoloRepository ruoloRepository;
 
 @Autowired
 private PersonaleRepository userRepository;
@@ -30,18 +21,24 @@ private PersonaleRepository userRepository;
 
     @GetMapping("/HomePage")
 
-    public String homePage(Model model) {
+    public String homePage(Model model, HttpSession session) {
+/*
         List<Personale> listaPersonale = (List<Personale>) userRepository.findAll();
         model.addAttribute("ruolo", "amministratore");
         model.addAttribute("personale", listaPersonale);
-        //model.addAttribute("ruolo", listaPersonale.get(1).getRuolo().toLowerCase());
-        //model.addAttribute("personale", ControllerPersonale.getListaPersonale());  // Questa riga serve per non perdere i dati della tabella del personale quando si torna alla homepage
+        model.addAttribute("ruolo", listaPersonale.get(1).getRuolo().toLowerCase());
+        model.addAttribute("personale", ControllerPersonale.getListaPersonale());  // Questa riga serve per non perdere i dati della tabella del personale quando si torna alla homepage
 
     //public String homePage(Model model, HttpSession session) {
-    //    Personale personale = (Personale) session.getAttribute("loggedUser");
-        //List<Ruolo> ruolo = ruoloRepository.findByPersonaleId(personale.getId()); questo era gia commentato prima del merge
-
-        //   model.addAttribute("ruolo", "amministratore"/*ruolo*/);
+    */
+        Personale personale = (Personale) session.getAttribute("utenteLoggato");
+        if (personale == null) {
+            return "redirect:/login";
+        }
+        //Optional<Ruolo> ruolo = ruoloRepository.findById(((Personale) session.getAttribute("userLoggato")).getRuolo().getNome()); //questo era gia commentato prima del merge
+        //String ruolo = ruoloRepository.findNomeRuoloByPersonaleId((((Personale) session.getAttribute("utenteLoggato")).getRuolo().getId())); //questo era gia commentato prima del merge
+        String ruolo = personale.getRuolo().getNome().toLowerCase();
+        model.addAttribute("ruolo", ruolo);
 
 
         return "HomePage";
